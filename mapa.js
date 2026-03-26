c1c3f126c713ccd7a4c5e33f0c4bded744c2a3a6
Jorge Peraza <jorgeph2013@gmail.com>
2026-03-25 21:53:36 -0700

    Preparando las bases del  proyecto

--- mapa.js
+++ mapa.js
@@ -1,9 +1,35 @@
 
-async function getLocation() {
+ /* function getUbicacion(){
+    const ubicacion = navigator.geolocation.getCurrentPosition((position)=>{
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log(lat)
+    console.log(lng)    
+   })
+ 
+ return ubicacion
+}
+getUbicacion()   */
+
+
+//
+navigator.geolocation.getCurrentPosition(
+  ({ coords: { latitude, longitude } }) => {
+    getLocation(latitude, longitude);
+  }
+);
+//
+
+
+async function getLocation(latitude, longitude) {
+  console.log(latitude)
+  console.log(longitude)
   const response = await fetch('https://opensheet.elk.sh/1lVrfhkCEHef29pcx2h2TVvJD9cqQY1GRh-YhTqABI_I/ubicacion');   
   const data = await response.json();
   
- let map = L.map('mapa').setView([32.485075,-116.810834], 16);
+ /* let map = L.map('mapa').setView([32.485075,-116.810834], 16); */
+ /* let map = L.map('mapa').setView([32.485075,-116.810834], 16); */
+ let map = L.map('mapa').setView([latitude, longitude], 16);
 
  var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -13,8 +39,16 @@ async function getLocation() {
  data.forEach((item)=>{
    if(!item.COORDENADAS) return
      const [lat, lng] = item.COORDENADAS.split(',').map(Number);
-     L.marker([lat, lng]).addTo(map).bindPopup(`${item.NO} / ${item.NOMBRE} / ${lat}, ${lng}`);
+     L.marker([lat, lng]).addTo(map).bindPopup(`<h1 class="red">hola</h1>${item.NO} / ${item.NOMBRE} / ${lat}, ${lng}`);
  }); 
 }
 
-getLocation();
+/* getLocation(); */
+
+
+/* const $btns = document.querySelector('.red');
+console.log($btns) */
+
+  
+
+
